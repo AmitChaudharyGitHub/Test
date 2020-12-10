766a0b47be7d4914a2c389eafd514800796bdc1c
unknown <mramit.singh89@gmail.com>
2020-12-10 22:19:27 +0530

    first commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentController :ControllerBase
+    {
+        private readonly int a;
+
+        public StudentController()
+        {
+        }
+
+        public string Get(){
+            return "Hello World";
+        }
+    }
+}
